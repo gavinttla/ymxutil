468ca7bf45969fae29aab9b1243edaa78e5823b1
Gavin B. Cui <GBCui@dm.local>
2017-09-14 18:00:00 -0700

    put in page parser feature/module

--- api/services/PageService.js
+++ api/services/PageService.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
 
 
 module.exports = {
@@ -9,9 +12,6 @@ module.exports = {
      */
     getLivePage: function(option, done) {
         console.log('exec getLivePage');
-
-        var objFs = require('fs');
-        var objRequest = require('request');
 
         if(!option.url || !option.file) {
             return false;
@@ -25,12 +25,14 @@ module.exports = {
             jar: true
         };
 
-        objRequest(requestOption, function (error, response, body) {
+        request(requestOption, function (error, response, body) {
             if (!error && response.statusCode == 200) {
 
+                var pageResult = this.checkPage(body);
+                /*
                 console.log(body);
 
-                objFs.writeFile("temp/"+option.file, body, function(error) {
+                fs.writeFile("temp/"+option.file, body, function(error) {
                     
                     if(error) {
                         console.log(error);
@@ -38,6 +40,7 @@ module.exports = {
 
                     console.log("store success");
                 });
+                */
             } else {
                 console.log("send request fail"+error);
             }
@@ -45,6 +48,69 @@ module.exports = {
         });
 
         
+    },
+
+
+    checkPage: function(html) {
+
+        var isKeyExist = this.isKeywordExist(html);
+
+        var nextPageUrl = this.getNextPageLink(html);
+
+        return {isKeyExist: isKeyExist, nextPageUrl: nextPageUrl};
+    },
+
+
+    getPageByUrl: function(url) {
+
+        var requestOption = {
+            url: 'https://www.amazon.com'+url,
+            headers: {
+                'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.5) Gecko/2008120122 Firefox/3.0.5'
+            },
+            jar: true
+        };
+
+
+        request(requestOption, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+
+            } else {
+                console.log("send request fail"+error);
+            }
+
+        });
+
+
+    },
+
+    isKeywordExist: function(html) {
+
+
+
+    },
+
+
+    /**
+     * load with cheerio and get the first pagination link
+     */
+    getNextPageLink: function(html) {
+        var $ = cheerio.load(html);
+        return $(".pagnLink a").attr('href');
+    },
+
+    /**
+     * check the current page number
+     */
+    getPageNum: function(pageUrl) {
+        var re = /page=(\d+)/i;
+        var found = $url.match(re);
+
+        if(found){
+            return found[1];
+        }else{
+            return false;
+        }
     },
 
     getCachePage: function(option, done) {
--- cmd.js
+++ cmd.js
@@ -1,5 +1,7 @@
 
 var objFs = require('fs');
+var parse5 = require('parse5');
+var cheerio = require('cheerio');
 
 function startParse() {
 
@@ -7,17 +9,35 @@ function startParse() {
 
     objFs.readFile('temp/ssd.html', 'utf8', function(err, data) {
 
+        var $ = cheerio.load(data);
+
+        console.log($(".pagnLink a").attr('href'));
+
+        var pagenum = getPageNum($(".pagnLink a").attr('href'));
+
         parseHtml(data);
 
     });
 
-    'temp/ssd.html'
+    //'temp/ssd.html'
 }
 
+function getPageNum($url) {
+
+    var re = /page=(\d+)/i;
+    var found = $url.match(re);
+
+    if(found){
+        return found[1];
+    }else{
+        return false;
+    }
+}
 
 function parseHtml(strHtml) {
 
-    console.log(strHtml);
+    //console.log(strHtml);
+    console.log('parseHtml');
 
 }
 
