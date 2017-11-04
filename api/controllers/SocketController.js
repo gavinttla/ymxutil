1f2e9c419ba4a7c8631f1b5c7b73fe917ab148ea
Gavin B. Cui <GBCui@dm.local>
2017-11-03 18:50:59 -0700

    complete the plan chart

--- api/controllers/SocketController.js
+++ api/controllers/SocketController.js
@@ -5,17 +5,38 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var Promise = require("bluebird");
+
+// the number is second
+function msgAfterTimeout (number) {
+  return new Promise((resolve, reject) => {
+      console.log("current running:"+number)
+      setTimeout(() => resolve(number+1), 1000)
+  })
+}
+
+var whilepromise = function(opromise) {
+  opromise.then(fun)
+}
+
 module.exports = {
 	
-
-
   /**
    * `SocketController.list()`
    */
   list: function (req, res) {
-    return res.json({
-      todo: 'list() is not implemented yet!'
-    });
+
+    var num = req.body.number
+
+    var i=0, p
+    while(i<num){
+      p = msgAfterTimeout(i)
+      p.then(function(newnum){
+        i = newnum
+      })
+    }
+
+
   },
 
 
